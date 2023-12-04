2066c1cbcebbec2d4b89b9d3b0b5a5d8e936594f
saumyabalina <141492226+saumyabalina@users.noreply.github.com>
2023-12-04 14:47:40 +0530

    Commit Q3

---
+++ q3/checkAudioFile.m
@@ -0,0 +1,12 @@
+%% Function 2: Check if signal is mono or stereo and 
+% change required sound accordingly
+
+function y = checkAudioFile(Y)
+    if(width(Y)==2)
+        % Input signal is stereo so taking mean along its dimension
+        y = mean(Y,width(Y));
+    else
+        % Input signal is mono so taking same signal
+        y = Y;
+    end
+end
